f8cf28097e83a2f6035c7424e98909161a002a20
olgasi <olgasi@OLGASI-LAP>
2018-05-10 11:21:38 +0300

    test

--- ClassLibrary1/UserManager.cs
+++ ClassLibrary1/UserManager.cs
@@ -9,15 +9,15 @@ namespace ClassLibrary
     /// </summary>
     public class UserManager: UserCollection
     {
-
         public UserManager() : base()
         {
             List < User > listOfUsers= new List<User>();
             CreateUser("1", "1", false, "125124", new DateTime(2008, 04, 14));
+            CreateUser("user2", "user2", false, "125124", new DateTime(2008, 04, 14));
+            CreateUser("admin", "admin", true, "125126", new DateTime(2008, 04, 14));
+            CreateUser("user3", "user3", false, "125124", new DateTime(2008, 04, 14));
             CreateUser("user", "user", false, "125124", new DateTime(2008,04,14));
-            CreateUser("user2", "user2", false, "125124", new DateTime(2008,04,14));
-            CreateUser("admin", "admin", true, "125126", new DateTime(2008,04,14));
-            CreateUser("user3", "user3", false, "125124", new DateTime(2008,04,14));
+           
         }
     }
 }
